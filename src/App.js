244f13378e21c45bda72b14811bf9c5c7b02729b
Firoj <mymindhungry@gmail.com>
2021-09-16 19:43:34 +0545

    Calculator in reactJS

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {add, sub, mult, div } from "./Calc";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+function App(){
+return(
+  <>
+  <ul>
+  <li>{add(30, 3)}</li>
+  <li>{sub(40, 4)}</li>
+  <li>{mult(50, 3)}</li>
+  <li>{div(40, 3)}</li>
+  </ul>
+
+  </>
   );
 }
-
 export default App;
